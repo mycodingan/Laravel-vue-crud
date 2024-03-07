2ed8984770a07fb44b041633f769b4369925702e
joshua <joshuapplg@gmail.com>
2024-03-07 08:48:19 +0700

    Installasi opertama

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+        <RouterLink to="/" class=" navbar-brand">Crud josshua wahyu</RouterLink>
+      </div>
+    <button
+    class=" navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarHeader">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarHeader">
+      <ul class=" navbar-nav ms-auto mb-2 mb-lg-0 d-flex">
+        <li class="nav-item">
+          <a class="nav-link" style="cursor: pointer;"> Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
+<script>
+export default {
+  data() {
+    return {
+      message_success_post: "",
+      user: null
+    }
+  }
+}
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container p-4">
+        <h1>login</h1>
+    </div>
+    <div class="container">
+        <form>
+            <div class="row">
+             <di class=" col-md-4">
+                <div class="form-group">
+                    <label class="form-label">ID card number
+                    </label>
+                    <input
+                        type="text"
+                        id="id"
+                        name="name"
+                        placeholder="placeholder"
+                        className="w-[300px] border border-slate-200 rounded-lg py-3 px-5 outline-none	bg-transparent"
+                    />
+                </div>
+             </di>
+            </div>
+        </form>
+    </div>
+</template>
